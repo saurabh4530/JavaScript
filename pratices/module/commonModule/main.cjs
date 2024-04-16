98dd84fd2b43c520a3281b339cbbc53114ac1610
saurabh4530 <kakadesaurabh453@gmail.com>
2024-04-16 10:23:01 +0530

    "2.0"

--- pratices/module/commonModule/main.cjs
+++ pratices/module/commonModule/main.cjs
@@ -1,12 +1,12 @@
-const sum = require('./operation.js');
+const sum = require('./operation.cjs');
 console.log("sum of given num: ",sum(1, 2)); // Output: 3 imported single file
 
-const div=require("./operation.js")
+const div=require("./operation.cjs")
 console.log("division :", div(6,3));
 
 
 //import multiple files
-let {multiplication,substraction}=require("./math.js")
+let {multiplication,substraction}=require("./math.cjs")
 console.log("multiplication: ", multiplication(25,20));
 console.log("substraction",substraction(10,5));
 
